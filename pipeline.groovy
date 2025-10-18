3df89f04240322b627de591801a17e35aae0937c
deb-alien <DevPro-2011@outlook.com>
2025-10-19 02:56:25 +0600

    Add Jenkins stages and steps

---
+++ pipeline.groovy
@@ -0,0 +1,57 @@
+pipeline {
+    agent any
+
+    environment {
+        // Define environment variables here
+        MY_ENV_VAR = 'Custom Value'
+    }
+
+    stages{
+        stage('Checkout') {
+            steps {
+                // Checkout code from version control
+                def gitRepoUrl = 'https://github.com/deb-alien/jenkins_pipeline.git'
+                checkout([$class: 'GitSCM', 
+                    branches: [[name: '*/main']], 
+                    userRemoteConfigs: [[url: gitRepoUrl]], 
+                    extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', noTags: false, shallow: true, depth: 1]]
+                ])
+            }
+        }
+
+        stage('Build') {
+            steps {
+                // Build your application here (e.g., compile, package, etc.)
+                sh '''
+                ls
+                echo "In Build Step"
+                '''
+            }
+        }
+
+        stage('Test') {
+            steps {
+                // Run your tests (e.g., unit tests, integration tests)
+                sh 'echo "In Test Step"'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                // Deploy your application to a target environment (e.g., staging, production)
+                sh 'echo "Value of ENV Variable is "$MY_ENV_VAR""'
+            }
+        }
+    }
+
+    post {
+        success {
+            // Actions to perform when the pipeline succeeds
+            echo 'Pipeline succeeded!'
+        }
+        failure {
+            // Actions to perform when the pipeline fails
+            echo 'Pipeline failed!'
+        }
+    }    
+}
